e1b30e499d570560610954e06599977bf6dbc72a
Pranjal-g083 <pranjal.baranwal@iitg.ac.in>
2023-03-06 10:42:32 +0530

    some changes

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
-#define KEYWORD                     257
-#define IDENTIFIER                  258
-#define INTEGER_CONSTANT            259
-#define CHAR_CONSTANT               260
-#define STRING_LITERAL              261
-#define PUNCTUATOR                  262
-#define SINGLE_LINE_COMMENT_START   263
-#define SINGLE_LINE_COMMENT_END     264
-#define MULTI_LINE_COMMENT_START    265
-#define MULTI_LINE_COMMENT_END      266
+#define KEYWORD                     260
+#define IDENTIFIER                  261
+#define INTEGER_CONSTANT            262
+#define CHAR_CONSTANT               263
+#define STRING_LITERAL              264
+#define PUNCTUATOR                  265
+#define SINGLE_LINE_COMMENT_START   266
+#define SINGLE_LINE_COMMENT_END     267
+#define MULTI_LINE_COMMENT_START    268
+#define MULTI_LINE_COMMENT_END      269
 extern char* yytext;
 extern int yylex();
 
@@ -18,41 +18,39 @@ int main()
     while(token==yylex())
     {
         switch(token){
-            case KEYWORD:{
+            case KEYWORD:
                 printf("<KEYWORD %d %s>\n", token,yytext);
                 break;
-            }
-            case IDENTIFIER:{
+            
+            case IDENTIFIER:
                 printf("<IDENTIFIER %d %s>\n", token,yytext);
                 break;
-            }case INTEGER_CONSTANT:{
+            case INTEGER_CONSTANT:
                 printf("<INTEGER_NSTANT %d %s>\n", token,yytext);
                 break;
-            }case CHAR_CONSTANT:{
+            case CHAR_CONSTANT:
                 printf("<CHARACTER CONSTANT %d %s>\n", token,yytext);
                 break;
-            }case STRING_LITERAL:{
+            case STRING_LITERAL:
                 printf("<String LITERAL %d %s>\n", token,yytext);
                 break;
-            }case PUNCTUATOR:{
+            case PUNCTUATOR:
                 printf("<PUNCTUATOR %d %s>\n", token,yytext);
                 break;
-            }case SINGLE_LINE_COMMENT_START:{
+            case SINGLE_LINE_COMMENT_START:
                 printf("<SINGLE LINE COMMENT START %d >\n", token);
                 break;
-            }case SINGLE_LINE_COMMENT_END:{
+            case SINGLE_LINE_COMMENT_END:
                 printf("<SINGLE LINE COMMENT END %d >\n", token);
                 break;
-            }case MULTI_LINE_COMMENT_START:{
+            case MULTI_LINE_COMMENT_START:
                 printf("<MULTI LINE COMMENT START %d>\n", token);
                 break;
-            }case MULTI_LINE_COMMENT_END:{
+            case MULTI_LINE_COMMENT_END:
                 printf("<MULTILINE COMMENT END %d >\n", token);
                 break;
-            }default: {
+            default: 
                 printf("INVALID TOKEN\n");
-            }
-
         }
     }
     return 0;
